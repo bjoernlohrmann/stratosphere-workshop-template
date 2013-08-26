398df6d69d38333d94e3e654aeb3e7a056597f2a
Teots <christian.wuertz@gmail.com>
2013-08-26 18:37:39 +0200

    Enabled the Grid to work with envelops directly.

--- src/main/java/de/komoot/hackathon/pactPlan/Gridify.java
+++ src/main/java/de/komoot/hackathon/pactPlan/Gridify.java
@@ -2,21 +2,17 @@ package de.komoot.hackathon.pactPlan;
 
 import java.util.List;
 
-import com.vividsolutions.jts.geom.GeometryFactory;
-
 import de.komoot.hackathon.Grid;
 import eu.stratosphere.pact.common.stubs.Collector;
 import eu.stratosphere.pact.common.stubs.MapStub;
 import eu.stratosphere.pact.common.type.PactRecord;
 import eu.stratosphere.pact.common.type.base.PactString;
 
-public class Gridify extends MapStub{
+public class Gridify extends MapStub {
 
-	private GeometryFactory geomFactory;
 	private Grid grid;
 	
 	public Gridify() {
-		geomFactory = new GeometryFactory();
 		grid = new Grid(0.1d);
 	}
 	
@@ -25,7 +21,7 @@ public class Gridify extends MapStub{
 			throws Exception {
 		
 		PactEnvelope env = record.getField(2, PactEnvelope.class);
-		List<String> ids = grid.getIdsForGeometry(geomFactory.toGeometry(env.getEnvelope()));
+		List<String> ids = grid.getIdsForGeometry(env.getEnvelope());
 		
 		for(String id : ids)
 		{
