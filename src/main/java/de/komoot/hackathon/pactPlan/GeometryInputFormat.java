f40c7f55b958be246fd25207a8afbc29d7b61fff
schernic <schernic@informatik.hu-berlin.de>
2013-08-26 17:15:11 +0200

    optimized GeometryInputFormat

--- src/main/java/de/komoot/hackathon/pactPlan/GeometryInputFormat.java
+++ src/main/java/de/komoot/hackathon/pactPlan/GeometryInputFormat.java
@@ -1,53 +1,72 @@
 package de.komoot.hackathon.pactPlan;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import de.komoot.hackathon.openstreetmap.GeometryModule;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
 import eu.stratosphere.pact.common.io.TextInputFormat;
 import eu.stratosphere.pact.common.type.PactRecord;
 import eu.stratosphere.pact.common.type.base.PactString;
 
 public class GeometryInputFormat extends TextInputFormat
 {
-	ObjectMapper mapper;
+	PactString id;
+	PactString geometry;
 	
-	public GeometryInputFormat()
-	{
-		mapper = new ObjectMapper();
-		mapper.registerModule(new GeometryModule());
-		mapper.configure(JsonGenerator.Feature.AUTO_CLOSE_TARGET, false);
-	}
 	
 	@Override
 	public boolean readRecord(PactRecord target, byte[] bytes, int offset, int numBytes)
 	{
-		boolean result = super.readRecord(target, bytes, offset, numBytes);
-		PactString text = target.getField(target.getNumFields()-1, PactString.class);
-		String textAsString = text.toString();
+		PactString str = this.theString;
 		
-//		try {
-//			JsonGeometryEntity entity = mapper.readValue(textAsString, JsonGeometryEntity.class);
-//			entity.getTags().clear();
-//		} catch (JsonParseException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (JsonMappingException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		if (this.ascii) {
+			str.setValueAscii(bytes, offset, numBytes);
+		}
+		else {
+			ByteBuffer byteWrapper = this.byteWrapper;
+			if (bytes != byteWrapper.array()) {
+				byteWrapper = ByteBuffer.wrap(bytes, 0, bytes.length);
+				this.byteWrapper = byteWrapper;
+			}
+			byteWrapper.clear();
+			byteWrapper.position(offset);
+			byteWrapper.limit(offset + numBytes);
+				
+			try {
+				CharBuffer result = this.decoder.decode(byteWrapper);
+				str.setValue(result);
+			}
+			catch (CharacterCodingException e) {
+				return false;
+			}
+		}
 		
-		textAsString = textAsString.substring(textAsString.indexOf(":") + 2);
-		String id = textAsString.substring(0, textAsString.indexOf("\""));
-		textAsString = textAsString.substring(textAsString.indexOf(":") + 2);
-		String geom = textAsString.substring(0, textAsString.indexOf("\""));
 		
-		target.setField(target.getNumFields()-1, new PactString(id));
-		target.addField(new PactGeometry(geom));
+		findStringValue(str,"id",id);
+		if(id == null)
+			throw new RuntimeException("Could not parse id attribute");
+		findStringValue(str,"geometry",geometry);
+		if(geometry == null)
+			throw new RuntimeException("Could not parse geometry attribute");
 		
-		return result;
+		target.clear();
+		target.addField(id);
+		target.addField(new PactGeometry(geometry.toString()));
+		return true;
+		
+	}
+	
+	public static void findStringValue(PactString text, String attribut, PactString target){
+		int startPosition = text.find("\""+attribut+"\":\"");
+		if(startPosition == -1){
+			target = null;
+			return;
+		}
+		startPosition += 4 + attribut.length();
+		int endPosition = text.find("\"",startPosition);
+		if(endPosition == -1){
+			target = null;
+			return;
+		}
+		text.substring(target,startPosition,endPosition);
 	}
 }
---
+++ src/test/java/de/komoot/hackathon/packPlan/GeometryInputFormatTest.java
@@ -0,0 +1,14 @@
+package de.komoot.hackathon.packPlan;
+
+import org.junit.*;
+
+import eu.stratosphere.pact.common.type.PactRecord;
+
+public class GeometryInputFormatTest {
+	@Test
+	public void test(){
+		PactRecord record = new PactRecord();
+		
+	}
+
+}
