5d0b85e6d5e42b8cfa2999219bef65d80ea3672b
Teots <christian.wuertz@gmail.com>
2013-08-26 15:35:20 +0200

    Made the NodesReducer more efficient by removing an unnecessary list.

--- src/main/java/de/komoot/hackathon/pactPlan/NodesReducer.java
+++ src/main/java/de/komoot/hackathon/pactPlan/NodesReducer.java
@@ -1,11 +1,11 @@
 package de.komoot.hackathon.pactPlan;
 
 import java.util.Iterator;
-import java.util.LinkedList;
 
 import eu.stratosphere.pact.common.stubs.Collector;
 import eu.stratosphere.pact.common.stubs.ReduceStub;
 import eu.stratosphere.pact.common.type.PactRecord;
+import eu.stratosphere.pact.common.type.base.PactList;
 import eu.stratosphere.pact.common.type.base.PactString;
 
 public class NodesReducer extends ReduceStub {
@@ -14,26 +14,18 @@ public class NodesReducer extends ReduceStub {
 	public void reduce(Iterator<PactRecord> records, Collector<PactRecord> out)
 			throws Exception {
 		PactRecord element = null;
-		LinkedList<PactString> areas = new LinkedList<PactString>();
+		PactList<PactString> areas = new PactListImpl();
+		
 		while (records.hasNext()) {
 			element = records.next();
-			
-			for(int i = 1; i < element.getNumFields(); i++)
-			{
-				PactString area = element.getField(i, PactString.class);
-				areas.add(area);
-			}			
+			areas.add(element.getField(1, PactString.class));						
 		}
 		
 		PactRecord result = new PactRecord();
-		
 		if(element != null)
 		{
 			result.addField(element.getField(0, PactString.class));
-			for(PactString area : areas)
-			{
-				result.addField(area);
-			}
+			result.addField(areas);
 			
 			out.collect(result);
 		}
